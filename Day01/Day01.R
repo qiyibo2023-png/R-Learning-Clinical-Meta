45a76a97cc0f39fd0553494f0efa502de70e6f3e
Yibo (Barry) Qi <qiyibo2023@gmail.com>
2026-05-08 13:02:27 -0400

    Add files via upload

---
+++ Day01/Day01.R
@@ -0,0 +1,23 @@
+# ========== Day01 R Basic Start ==========
+# 1. Two assignment methods in R
+a <- 10
+b = 5
+
+# 2. Basic arithmetic calculation
+a + b   # Addition 
+a - b   # Subtraction 
+a * b   # Multiplication 
+a / b   # Division 
+a ^ 2   # Square 
+
+# 3. Character / text variable
+name <- "Clinical Psychology R Learning"
+name
+
+# 4. Logical judgement: return TRUE / FALSE
+a > b
+a < b
+a == b
+
+# 5. List all variables in environment
+ls()
